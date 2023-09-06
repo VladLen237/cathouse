f5fa1434324943110bfd5d71ce4a53a7722426e2
VladLen237 <144188711+VladLen237@users.noreply.github.com>
2023-09-06 13:00:08 +1000

    Add files via upload

---
+++ about.php
@@ -0,0 +1,99 @@
+<html>
+    <head>
+<title>About</title>
+ <link href="about.css" rel="stylesheet">
+  <link href="allstyle.css" rel="stylesheet">
+  <script src="mainscript.js"></script>
+  
+    </head>
+        <body>
+		
+		
+              <header>
+			<div class="logo"></div><div class="nazv_syta"><h1><a href="main.php">Kekich хаус</a></h1></div>
+			
+		<div class="but-menu">
+		<div class="but_m"><a href="main.php">Главная</a></div>
+		<div class="but_m"><a href="about.php">О нас</a></div>
+		<div class="but_m"><a href="prices.php">Номера и цены</a></div>
+		<div class="but_m"><a href="new.php">Забронировать</a></div>
+</div>
+			</header>
+          
+             
+                <main>
+				<div class="l_ab">
+				<div class="prei">
+				<h3><div class="ch">3</div></h3>
+				<p>Года</p>
+				</div>
+				<div class="prei">
+				<h3><div class="ch">45</div></h3>
+				<p>Мест</p>
+				</div>
+				<div class="prei">
+				<h3><div class="ch">7<div></h3>
+				<p>Вет-Специалистов</p>
+				</div>
+				</div>
+                    <div class="block111"><h2>О нас</h2>
+					<div class="cont_txt">
+					Наша гостиница для кошек "Kekich House" предоставлет услуги по вольерной передержке. 
+				В качестве дополнительного сервиса питомцам будет доступен целый ряд бесплатных услуг, что является
+				одним из основных конкурентных преимуществ проекта наряду с высоким уровнем сервиса. Гостиница для кошек работает в среднем ценовом сегменте, 
+				при качестве услуг – выше среднего. 
+					</div>
+					
+					<div class="cont_img" style="background-image:url('about.png');"></div>
+					
+					</div>
+					
+					
+                    <div class="block2" style="height:83vh;"><h2>Услуги</h2>
+					<div class="cont_txt1">
+		
+				<ul>Услуги входящие в стоимость аренды номера<li>Ежедневный контроль ветеринарного врача</li><li>Чистка ушей
+
+</li><li>Обработка глаз</li><li>Уборка дважды в день</li><li>Информирование хозяина о состоянии и поведении питомца</li><li>Лечение питомца с привлечением ветеринара</li></ul>
+					
+					</div>
+					</div>
+					<div class="block4">
+					<h2>Условия соержания</h2>
+					<p>Животных содержат в индивидуальных номерах, они изолированы друг от друга, не имеют визуального и
+					воздушного контакта. Полы исключают возможности сделать норы или подкоп, сделаны из плитки, наливными или из
+					прочих антивандальных материалов. Полы и стены хорошо и быстро очищаемые и приспособленны для уборки моющими и дезинфицирующими 
+					средствами. В каждом номере присутствует естественное и искусственное освещение и вентиляция. Температура поддерживается
+					от 16 до 25 С. Номера для кошек оборудованы когтеточками, приспособлениями для лазанья.</p>
+					<div class="cont_img1" style="background-image:url('люкс.png');"></div>
+					</div>
+                    <div class="block3">
+					<h2>Видео-обзор</h2>		
+  
+  <iframe width="70%" height="450" src="https://www.youtube.com/embed/-qo-OsrKr48" title="Номер глазами кота" frameborder="0" 
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"autoplay muted playsinline></iframe>
+ 
+</div>
+
+<div class="block4">
+					<section class="map">
+    <div class="container">
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2645.047878230029!2d135.07486511530865!3d48.47479523559316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5efae98b08a3762d%3A0xbc5fd43804337d3d!2z0YPQuy4g0JPQvtCz0L7Qu9GPLCA1LCDQpdCw0LHQsNGA0L7QstGB0LosINCl0LDQsdCw0YDQvtCy0YHQutC40Lkg0LrRgNCw0LksIDY4MDAzMA!5e0!3m2!1sru!2sru!4v1669010938910!5m2!1sru!2sru" 
+allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</section>
+</div>
+                </main>
+                <footer>
+Структура сайта и содержание были частично взяты с таких сайтов как:
+<ul>
+<li><a href="https://sircat.ru/">Sircat</a></li>
+<li><a href="https://kotville.ru">Kotville</a></li>
+<li><a href="https://cat-town.ru">Cat-Town</a></li>
+<li><a href="https://bookingcat.ru/"></a>BookingCat</li>
+</ul>
+				</footer>
+
+    </body>
+	
+</html>
---
+++ mainscript.js
@@ -0,0 +1,20 @@
+function openMenu(){
+	document.getElementById("sidebar").classList.toggle('active');
+	
+}
+/*
+var left = document.querySelector('.left');
+var right = document.querySelector('.right');
+var viewport = document.querySelector('.viewport');
+left.addEventListener('click', function() {
+   viewport.scrollBy({
+      left: -1210,
+      behavior: 'smooth'
+   });
+});
+right.addEventListener('click', function() {
+   viewport.scrollBy({
+      left: 1210,
+      behavior: 'smooth'
+   });
+});*/
